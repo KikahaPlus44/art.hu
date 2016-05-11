6df68c9c0c6fa33911e076f34845639ac6b71953
KikahaPlus44 <KikahaPlus44>
2016-05-11 07:53:34 +0300

    newwave

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
-	console.log( "ready!" );
-})
+	var $grid = $('.grid').masonry();
+	// open close card
+	$('.open-js').click(function(){
+		if ($('.element-item').hasClass('open')) {
+			$('.element-item').removeClass('open'); //closed open card
+			$(this).parent().addClass('open');// opend current card
+			$grid.masonry('layout');// re masonry
+		} else {
+			$(this).parent().toggleClass('open');//toggle opend card
+			$grid.masonry('layout');// re masonry
+		};
+	});
+
+	// next prew card
+	$('.next-js').click(function(){
+		$(this).parent().removeClass('open');
+		$(this).parent().next().addClass('open');
+		$grid.masonry('layout');// re masonry
+	});
+	$('.prev-js').click(function(){
+		$(this).parent().removeClass('open');
+		$(this).parent().prev().addClass('open');
+		$grid.masonry('layout');// re masonry
+	});
+
+
+	console.log($('.element-item').hasClass('open'));
+});
 
